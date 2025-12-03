ebbe370dfbc5ec917d8d269466765ac5baa6dc49
Connor Chen <connorchen@berkeley.edu>
2025-12-02 20:27:31 -0800

    update

--- white_agent/__init__.py
+++ white_agent/__init__.py
@@ -5,7 +5,7 @@ This package contains the white agent implementations for Terminal Bench.
 The white agent is responsible for solving terminal bench problems.
 """
 
-from .agent import TerminalBenchAgent, A2ATerminalBenchServer, Agent
+from .agent import TerminalBenchAgent, A2ATerminalBenchServer
 from .a2a_protocol import (
     Message, Task, TaskStatus, Artifact,
     TextPart, Part, AgentCard
